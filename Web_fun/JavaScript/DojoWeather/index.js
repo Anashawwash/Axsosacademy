3c54897603e75c3f302287f8f3332edfe9e19b05
Anas hawwash <hawwashanas@gmail.com>
2025-09-09 16:01:23 +0300

    hope

---
+++ Web_fun/JavaScript/DojoWeather/index.js
@@ -0,0 +1,30 @@
+function printLoding() {
+    alert("loading weather Report ")
+}
+
+function rmDiv() {
+    var div = document.querySelector(".footer");
+    div.remove();
+}
+
+function changeTo(element) {
+    if (element.value == "C") {
+        const array = document.querySelectorAll(".converter")
+        for (var i = 0 ; i <array.length ; i ++){
+            var end_val = parseInt(array[i].textContent);
+            var last = (end_val - 32) * 5/9 ;
+            array[i].textContent = Math.round(last) + "℃"
+        }
+
+
+    }
+    else {
+        const array = document.querySelectorAll(".converter");
+        for (var i = 0; i < array.length; i++) {
+            var end_val = parseInt(array[i].textContent);
+            var last = (end_val * 9/5 ) + 32 ;
+            array[i].textContent = Math.round(last) +"F"
+        }
+    }
+}
+
