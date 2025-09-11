88f644944f01172204cbca357e3a4bc955705828
Anas hawwash <hawwashanas@gmail.com>
2025-09-11 12:58:58 +0300

    hope

--- Web_fun/JavaScript/Likes/index.js
+++ Web_fun/JavaScript/Likes/index.js
@@ -1,6 +1,6 @@
-var likes = [9,12,9];
-var names = ["Neil" ,"Nichole","Jim"];
 
 function addLike(num){
-document.getElementById(names[num]).innerText = likes[num] ++;
+    var likesAcc = document.getElementById(num).innerText
+    likesAcc ++ ;
+    document.getElementById(num).innerText = likesAcc;
 }
