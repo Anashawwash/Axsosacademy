79914d1a9d7f752f8819ce7f6f3cc74987c400df
Anas hawwash <hawwash.anas@gmail.com>
2025-10-28 16:09:37 +0200

    java

--- java/Cafe_java/Cafe.java
+++ java/Cafe_java/Cafe.java
@@ -1,50 +1,63 @@
-public class Cafe {
+public class CafeJava {
 
 
     public static void main(String[] args) {
         // APP VARIABLES
         // Lines of text that will appear in the app.
-        double dripcofe =10.5;
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = "your order will be ready shortly";
+        String readyMessage = "your order is ready";
+        String displayTotalMessage = "Your total is $";
+
+        // Menu variables (add yours below)
+        double dripCoffe = 10.5;
         double latte = 12;
         double cappuccino = 10.1;
 
-        String generalGreeting = "Welcome to Cafe Java, ";
-        String pendingMessage = ", your order will be ready shortly";
-        String readyMessage = ", your order is ready";
-        String displayTotalMessage = "Your total is $";
-        
-        // Menu variables (add yours below)
-        double mochaPrice = 3.5;
-    
         // Customer name variables (add yours below)
-        String customer1 = "ADAM";
-        String customer2 = "Ahmad";
-        String sustomer3 = "sali";
-        
-    
+        String customer1 = "Ahmad";
+        String customer2 = "sali";
+        String customer3 = "Adam";
+
+
         // Order completions (add yours below)
-        boolean OrderStatus1 = false;
-        boolean OrderStatus2 = true;
-        boolean OrderStatus3 = true;
 
-        
-        if (OrderStatus2) {
-            System.out.println(cappuccino);
+        boolean orderStatus1 = false;
+        boolean orderStatus2 = true;
+        boolean orderStatus3 = true;
+
+
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        // ** Your customer interaction print statements will go here ** //
+        System.out.println(generalGreeting + customer1);
+        System.out.println(generalGreeting + customer2);
+        System.out.println(generalGreeting + customer3);
+
+        if (orderStatus2) {
+            System.out.println(readyMessage);
+        } else {
+            System.out.println(pendingMessage);
         }
-            System.out.println(latte);
-        if (OrderStatus3){
-            System.out.println(OrderStatus3);
+        if (orderStatus1) {
+            System.out.println(readyMessage);
+        } else {
+            System.out.println(pendingMessage);
         }
-        System.out.println(latte + dripcofe);
+        System.out.println("Sali ordered 2 lattes "+ displayTotalMessage +(latte + latte));
+        if (orderStatus2) {
+            System.out.println(readyMessage);
+        } else {
+            System.out.println(pendingMessage);
+        }
+        System.out.println(displayTotalMessage + (latte + dripCoffe) + " What he owes " + (latte - dripCoffe));
+        if (orderStatus3) {
+            System.out.println(readyMessage);
+        } else {
+            System.out.println(pendingMessage);
+        }
 
-
-
-
-
-
-    
-        System.out.println("The total: " + (latte + dripcofe) + " What he owes " + (latte - dripcofe));
- 
     }
 }
 
+ 
