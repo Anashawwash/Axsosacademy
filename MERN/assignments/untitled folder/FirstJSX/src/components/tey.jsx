4797d25728a87b33ba14828a9673e20ae87c86de
Anas hawwash <hawwashanas@gmail.com>
2025-12-04 15:33:23 +0200

    react

---
+++ MERN/assignments/untitled folder/FirstJSX/src/components/tey.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const header = ()=>{
+
+    return (
+        <>
+        <h1>Hello Dojo</h1>
+        <h1>hings i need to do</h1>
+        <ul>
+            <li>learn react</li>
+            <li>climb </li>
+            <li>run a </li>
+            <li>feed the dogs </li>
+        </ul>
+        </>
+    )
+}
+
+export default header;
