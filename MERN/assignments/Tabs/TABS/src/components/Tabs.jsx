483c236eb48043bf1e4ac982e19fb36443698bff
Anas hawwash <hawwashanas@gmail.com>
2025-12-08 21:13:12 +0200

    react

--- MERN/assignments/Tabs/TABS/src/components/Tabs.jsx
+++ MERN/assignments/Tabs/TABS/src/components/Tabs.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 
 const Tabs = props =>{
+    const [one,setOne] = useState(false)
+    const [two,setTwo] = useState(false)
+    const [three,setThree] = useState(false)
     const chaneContant = (e,type)=> {
-        const dive = document.getElementById("main")
-        console.log(dive);
-        const newp = document.getElementById("con")
-
         document.getElementById("one").classList.remove("bg-black");
         document.getElementById("two").classList.remove("bg-black");
         document.getElementById("three").classList.remove("bg-black");
@@ -16,15 +15,18 @@ const Tabs = props =>{
         e.target.classList.add("bg-black");
         e.target.classList.add("text-white");
 
-
         if (type === 1){
-            newp.textContent = "Tab 1 content is showing here";
-        }
-        else if (type === 2){
-            newp.textContent = "Tab 2 content is showing here";
-        }
-        else if (type === 3){
-            newp.textContent = "Tab 3 content is showing here";
+            setOne(true)
+            setTwo(false)
+            setThree(false)
+        }else if (type === 2){
+            setOne(false)
+            setTwo(true)
+            setThree(false)
+        }else if (type === 3){
+            setOne(false)
+            setTwo(false)
+            setThree(true)
         }
     }
 
@@ -35,8 +37,10 @@ const Tabs = props =>{
         <button id="two" onClick={(e)=>chaneContant(e,2)} className={"text-2xl mr-3 border-2 w-40 hover:bg-black hover:text-white"}>Tab 2</button>
         <button id="three" onClick={(e)=>chaneContant(e,3)} className={"text-2xl mr-3 border-2 w-40 hover:bg-black hover:text-white"}>Tab 3</button>
     </div>
-    <div id="main" className={"border-2 h-100 w-150 mx-auto"}> 
-        <p id="con"></p>
+    <div className={"border-2 h-100 w-150 mx-auto"}> 
+        {one && (<p>Tab 1 content is showing here</p>)}
+        {two && (<p>Tab 2 content is showing here</p>)}
+        {three && (<p>Tab 3 content is showing here</p>)}
     </div>
     </>)
 }
